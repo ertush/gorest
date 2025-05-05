1b1fc28bc7dbac73ad671a6f3607317c1440a103
ertush <erotush77@gmail.com>
2025-05-05 14:59:09 +0300

    renamed package routes to views

--- main.go
+++ main.go
@@ -2,24 +2,29 @@ package main
 
 import (
 	"github/ertush/gorest/database"
-	"github/ertush/gorest/routes"
+	"github/ertush/gorest/views"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func welcome(c *fiber.Ctx) error {
-	return c.Status(200).SendString("Hello from go fiber")
-}
+func loadviews(app *fiber.App) {
 
-func loadRoutes(app *fiber.App) {
+	// User Routes
 
-	app.Get("/", welcome)
-	app.Post("/api/user", routes.CreateUser)
-	app.Get("/api/users", routes.GetUsers)
-	app.Get("/api/users/:id", routes.GetUser)
-	app.Put("/api/user", routes.UpdateUser)
-	app.Delete("/api/users/:id", routes.DeleteUser)
+	app.Post("/api/user", views.CreateUser)
+	app.Get("/api/users", views.GetUsers)
+	app.Get("/api/users/:id", views.GetUser)
+	app.Put("/api/user", views.UpdateUser)
+	app.Delete("/api/users/:id", views.DeleteUser)
+
+	// Product Routes
+
+	app.Post("/api/product", views.CreateProduct)
+	app.Get("/api/products", views.GetProducts)
+	app.Get("/api/products/:id", views.GetProduct)
+	app.Put("/api/product", views.UpdateProduct)
+	app.Delete("/api/products/:id", views.DeleteProduct)
 
 }
 
@@ -29,7 +34,7 @@ func main() {
 
 	app := fiber.New()
 
-	loadRoutes(app)
+	loadviews(app)
 
 	log.Fatalln(app.Listen(":4000"))
 }
