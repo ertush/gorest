5622a52969f9da0d9274a430e30c24ac8cd11f19
ertush <erotush77@gmail.com>
2025-05-05 15:11:35 +0300

    Updated Product Serializer

--- serializers/product.go
+++ serializers/product.go
@@ -4,4 +4,5 @@ type Product struct {
 	ID           uint   `json:"id" gorm:"primaryKey"`
 	Name         string `json:"name"`
 	SerialNumber string `json:"serial_number"`
+	User         int    `json:"user_id"`
 }
