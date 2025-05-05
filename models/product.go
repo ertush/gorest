9c3e5c9ea640467de76ef1befea4baef65367e46
ertush <erotush77@gmail.com>
2025-05-05 15:04:09 +0300

    Modified Product model

--- models/product.go
+++ models/product.go
@@ -1,10 +1,17 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Product struct {
+	gorm.Model
 	ID           uint      `json:"id" gorm:"primaryKey"`
 	Name         string    `json:"name"`
 	SerialNumber string    `json:"serial_number"`
+	UserRefer    int       `json:"user_id" gorm:"user_id"`
+	User         User      `gorm:"foreignKey:UserRefer"`
 	CreatedAt    time.Time `json:"created_at"`
 }
