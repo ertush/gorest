581b4717d3450a27c718254eadcbf117cf3b4bca
ertush <erotush77@gmail.com>
2025-05-29 17:12:10 +0300

    Updated database.go

--- database/database.go
+++ database/database.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/lpernett/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -18,14 +19,12 @@ var Database DbInstance
 
 func ConnectDB() {
 
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	dsn := os.Getenv("DSN") // fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Nairobi", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
-
-	// log.Println("dsn: " + dsn)
+	dsn := os.Getenv("DSN")
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
