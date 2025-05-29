a28fc66b12451292e3212b5ff2a028acf3405548
ertush <erotush77@gmail.com>
2025-05-29 15:22:30 +0300

    Removed the need to read .env file

--- database/database.go
+++ database/database.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/lpernett/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -19,14 +18,14 @@ var Database DbInstance
 
 func ConnectDB() {
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	dsn := os.Getenv("DSN") // fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Nairobi", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 
-	log.Println("dsn: " + dsn)
+	// log.Println("dsn: " + dsn)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
